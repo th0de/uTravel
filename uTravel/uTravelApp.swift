d84e26ddc49f94b55119005889830eb98a7c96d1
th0de <elijahthode@gmail.com>
2025-03-04 15:36:26 -0600

    Initial Commit

---
+++ uTravel/uTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  uTravelApp.swift
+//  uTravel
+//
+//  Created by James Flair on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct uTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
